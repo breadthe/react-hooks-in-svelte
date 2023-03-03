1f309880c5d24f5358c496e52423fa91fbd64aab
breadthe <breadthe@users.noreply.github.com>
2023-03-03 09:41:38 -1000

    init

---
+++ src/lib/store.ts
@@ -0,0 +1,4 @@
+
+import { writable, type Writable } from 'svelte/store';
+
+const storeValue: Writable<number> = writable(1);
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-modern.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Skeleton</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/breadthe/react-vs-svelte"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
