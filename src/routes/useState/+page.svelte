7d722183cab28da49b8cd0d990af2a6f97ad4e63
breadthe <breadthe@users.noreply.github.com>
2023-03-03 22:24:55 -1000

    POC for importing react vs svelte code in 2 panes

--- src/routes/useState/+page.svelte
+++ src/routes/useState/+page.svelte
@@ -1,1 +1,23 @@
-<h1>useState</h1>
+<script>
+	import Icon from "../../lib/Icon.svelte"
+	import react from "./react.tsx?raw"
+	import svelte from "./svelte.svelte?raw"
+</script>
+
+<h1 class="m-4 sm:m-8">useState</h1>
+
+<div class="flex flex-col sm:flex-row gap-8 p-4 sm:p-8">
+	<div class="w-full sm:w-1/2 flex flex-col gap-2 sm:gap-4">
+		<h2 class="flex items-center gap-2"><Icon icon="react" /> React</h2>
+
+		<pre><code>{react}</code></pre>
+	</div>
+	<div class="w-full sm:w-1/2 flex flex-col gap-2 sm:gap-4">
+		<h2 class="flex items-center gap-2"><Icon icon="svelte" /> Svelte</h2>
+
+		<pre><code>{svelte}</code></pre>
+	</div>
+</div>
+
+<style>
+</style>
---
+++ src/routes/useState/svelte.svelte
@@ -0,0 +1,8 @@
+<script>
+	let count = 0
+</script>
+
+Count: {count}
+<button on:click={() => (count = 0)}>Reset</button>
+<button on:click={() => count--}>-</button>
+<button on:click={() => count++}>+</button>
