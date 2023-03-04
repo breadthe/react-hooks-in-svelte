a43595c152301844892cb04fadd871118f88093e
breadthe <breadthe@users.noreply.github.com>
2023-03-03 23:05:08 -1000

    React/Svelte headers styling

--- src/routes/useState/+page.svelte
+++ src/routes/useState/+page.svelte
@@ -9,12 +9,12 @@
 
 <div class="flex flex-col sm:flex-row gap-8 p-4 sm:p-8">
 	<div class="w-full sm:w-1/2 flex flex-col gap-2 sm:gap-4">
-		<h2 class="flex items-center gap-2"><Icon icon="react" /> React</h2>
+		<h2 class="flex items-center gap-2 px-4 py-2 rounded-full bg-gradient-to-r from-react to-react/10 text-blue-900"><Icon icon="react" /> React</h2>
 
 		<CodeBlock code={react} language="jsx" />
 	</div>
 	<div class="w-full sm:w-1/2 flex flex-col gap-2 sm:gap-4">
-		<h2 class="flex items-center gap-2"><Icon icon="svelte" /> Svelte</h2>
+		<h2 class="flex items-center gap-2 px-4 py-2 rounded-full bg-gradient-to-r from-svelte to-svelte/10 text-orange-50"><Icon icon="svelte" /> Svelte</h2>
 
 		<CodeBlock code={svelte} language="svelte" />
 	</div>
